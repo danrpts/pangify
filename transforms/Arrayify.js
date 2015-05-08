0bb264066ff74b7736afb172506b72fad14db8af
leinad <danrpts@gmail.com>
2015-05-07 20:16:12 -0700

    known bug in arrayify parsing

--- transforms/Arrayify.js
+++ transforms/Arrayify.js
@@ -14,7 +14,8 @@ module.exports = through2.ctor(function (token, enc, callback) {
     case "polygon":
       var lexemes = token.lexeme.split(/[, \t]+/);
       lexemes.forEach(function(item, index) {
-        if (/[\w.]+/.test(item)) lexemes[index] = parseInt(item);
+        // bug here
+        if (/\d+/.test(item)) lexemes[index] = parseInt(item);
       });
       this.store.push(lexemes);
       break;
